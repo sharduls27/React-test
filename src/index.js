364e11fcff53c2209c1b44f132225e499705a6a1
SHARDUL SHIVAM <55155099+sharduls27@users.noreply.github.com>
2023-04-24 12:51:18 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul></ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
